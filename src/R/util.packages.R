28f45a8ceec369709ce23ec5d50a27a5e780159b
lis262 <shangzhong.li@pfizer.com>
2022-02-10 17:01:40 +0000

    update softwares

---
+++ src/R/util.packages.R
@@ -0,0 +1,6 @@
+packages = c('dplyr','ggplot2','DESeq2','glmpca','sva','pheatmap',
+              'RColorBrewer','EnhancedVolcano','org.Mm.eg.db',
+             'org.Hs.eg.db','PCAtools')
+BiocManager::install(packages)
+
+
