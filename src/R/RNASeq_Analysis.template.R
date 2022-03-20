c3ef63b480e3562c9735bd4dc02593d3958d8397
Lei Shang <SHANGL02@pfizer.com>
2022-03-19 21:33:10 -0400

    Non-essential changes

--- src/R/RNASeq_Analysis.template.R
+++ src/R/RNASeq_Analysis.template.R
@@ -33,7 +33,7 @@ process.sampleVariance.all = function(cts.mat, sample.meta, variables,min_count=
   tryCatch(
     {
       print("Generating pdf")
-      pdf(file='SampleVariance.all.pdf')
+      pdf(file='SampleVariance.all.pdf', width=10)
       p1<-plot.mds(dge.filter, variables)
       p2<-plot.glmpca(dds, variables)
       p3<-plot.vst(dds,vsd, rld)
@@ -72,7 +72,6 @@ cts.mat = read.combined_cts(cts_file)
 
 dim(cts.mat)
 
-
 ## Load genome annotation file
 #gtf.file <- 'X:\\projects\\Ensembl\\release-98\\gtf\\Mus_musculus.GRCm38.98.chr.gtf'
 #gene_anno = load.gtf(gtf.file)
@@ -83,10 +82,16 @@ setwd(outdir)
 
 ## Load sample metadata
 sample.meta <- read.csv(sample.meta.file, sep='\t', header=TRUE, stringsAsFactors = FALSE)
-sample.meta$mergeCond = pasteMultiCol(sample.meta, variables, ':')
+sample.meta$mergeCond = pasteMultiCol(sample.meta, variables, '.')
 dim(sample.meta)
 
-## Verify
+## add filter of based on sample metadata
+sample.meta=sample.meta[sample.meta$Quality=='good',]
+cts.mat = cts.mat[,colnames(cts.mat) %in% sample.meta$Sample]
+dim(cts.mat)
+dim(sample.meta)
+
+## Verify count table and sample metadata
 if (!verify.meta(cts.mat, sample.meta)) {
   print("Ordering columns of count matrix")
   cts.mat<-cts.mat[,unlist(sample.meta[1])]
@@ -116,4 +121,8 @@ compare_df = read.table(comparison.file,header=T, sep="\t")
 process.sampleVariance.all(cts.mat, sample.meta, variables)
 
 ## DE analysis
-DE.DESeq(cts.mat, sample.meta, compare_df, min_total_count, outdir, lfc_cutoff, alpha, topN, species)
+if (exists("DE_algo") && DE_algo=="limma") {
+  DE.limma(cts.mat, sample.meta, compare_df, min_count, min_total_count, outdir, lfc_cutoff, alpha, topN, species)
+} else {
+  DE.DESeq(cts.mat, sample.meta, compare_df, min_total_count, outdir, lfc_cutoff, alpha, topN, species)
+}
