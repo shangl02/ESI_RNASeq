ce5dc050bd1efe4c1a77a9eeefbffe5fbe763bc0
lis262 <shangzhong.li@pfizer.com>
2022-06-24 22:33:08 +0000

    add additional packages

--- src/R/util.packages.R
+++ src/R/util.packages.R
@@ -1,8 +1,9 @@
 # this file defines softwares that need to be installed
 packages = c('dplyr','ggplot2','DESeq2','glmpca','sva','pheatmap',
-              'RColorBrewer','EnhancedVolcano','org.Mm.eg.db',
+             'RColorBrewer','EnhancedVolcano','org.Mm.eg.db',
              'org.Hs.eg.db','PCAtools','RUVSeq','gplots','stringr',
-             'limma','matrixStats','MASS','pheatmap','viridis','GOfuncR')
+             'limma','matrixStats','MASS','pheatmap','viridis','GOfuncR',
+             'clusterProfiler','apeglm','gage','gageData','forcats',
+             'ggnewscale','ggridges','msigdbr','ggupset')
+
 BiocManager::install(packages)
-
-
