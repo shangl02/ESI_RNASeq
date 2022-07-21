4d1e61d414ad16f98407f29ece367907e423b800
lis262 <shangzhong.li@pfizer.com>
2022-07-21 14:35:07 +0000

    add package to install

--- src/R/util.packages.R
+++ src/R/util.packages.R
@@ -1,7 +1,7 @@
 # this file defines softwares that need to be installed
 packages = c('dplyr','ggplot2','DESeq2','glmpca','sva','pheatmap',
              'RColorBrewer','EnhancedVolcano','org.Mm.eg.db',
-             'org.Hs.eg.db','PCAtools','RUVSeq','gplots','stringr',
+             'org.Hs.eg.db','org.Rn.eg.db','PCAtools','RUVSeq','gplots','stringr',
              'limma','matrixStats','MASS','pheatmap','viridis','GOfuncR',
              'clusterProfiler','apeglm','gage','gageData','forcats',
              'ggnewscale','ggridges','msigdbr','ggupset')
