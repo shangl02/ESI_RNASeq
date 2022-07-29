4f912ee9d2a864dcd23b03a25a118728b13e50c1
lis262 <shangzhong.li@pfizer.com>
2022-07-29 15:31:11 +0000

    go back to original path after running

--- src/R/util.D4C.R
+++ src/R/util.D4C.R
@@ -26,11 +26,13 @@ run_d4c_for_raw_count = function(cts.mat, sample.meta, compare_df, domain, user,
       write.table(compare_df[i,], sub_compare_fn, sep='\t',quote=F, row.names=F)
       
       # 3rd part, run the d4c api
+      current_path = getwd()
       setwd(d4c_code_path)
       code = 'run_data4cure_for_raw_RNASeq.py'
       cmd = glue("python3 {code} -i {sub_count_fn} -m {sub_meta_fn} -c {sub_compare_fn} -v mergeCond -d count --domain {domain} --user {user} --pw {pwd} --token {token}")
       cmd = gsub('\\\\','/',cmd)
       system(cmd)
+      setwd(current_path)
     },
     error = function(err) {
       print(paste("Error:", err))
@@ -62,12 +64,14 @@ run_d4c_for_deseq2_results = function(compare_df, domain, user, pwd, token='none
       file.copy(deseq2_fn,cp_deseq2_fn)
       
       # 3rd part, run the d4c api
+      current_path = getwd()
       setwd(d4c_code_path)
       code = 'run_data4cure_for_DESeq2_results.py'
       deseq2_tbl = glue('{path}/{comparison}.result.tbl')
       cmd = glue("python3 {code} -i {cp_deseq2_fn} -o {deseq2_tbl} --domain {domain} --user {user} --pw {pwd} --token {token}")
       cmd = gsub('\\\\','/',cmd)
       system(cmd)
+      setwd(current_path)
       
     },
     error = function(err) {
